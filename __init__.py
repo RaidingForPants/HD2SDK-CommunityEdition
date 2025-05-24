322d55fc210150b10ed1b925893235b0555bd548
BiscuitDev <86505354+Boxofbiscuits97@users.noreply.github.com>
2025-05-23 20:39:42 -0400

    Auto ID Swapping

--- __init__.py
+++ __init__.py
@@ -597,7 +597,8 @@ def CreateModel(model, id, customization_info, bone_names, transform_info, bone_
         # set object properties
         new_object["MeshInfoIndex"] = mesh.MeshInfoIndex
         new_object["BoneInfoIndex"] = mesh.LodIndex
-        new_object["Z_ObjectID"]      = str(id)
+        new_object["Z_ObjectID"] = str(id)
+        new_object["Z_SwapID"] = ""
         if customization_info.BodyType != "":
             new_object["Z_CustomizationBodyType"] = customization_info.BodyType
             new_object["Z_CustomizationSlot"]     = customization_info.Slot
@@ -3488,10 +3489,11 @@ def CheckDuplicateIDsInScene(self, objects):
     custom_objects = {}
     for obj in objects:
         obj_id = obj.get("Z_ObjectID")
+        swap_id = obj.get("Z_SwapID")
         mesh_index = obj.get("MeshInfoIndex")
         bone_index = obj.get("BoneInfoIndex")
         if obj_id is not None:
-            obj_tuple = (obj_id, mesh_index, bone_index)
+            obj_tuple = (obj_id, mesh_index, bone_index, swap_id)
             try:
                 custom_objects[obj_tuple].append(obj)
             except:
@@ -4252,11 +4254,21 @@ class SaveStingrayMeshOperator(Operator):
             return {'CANCELLED'}
         if MeshNotValidToSave(self):
             return {'CANCELLED'}
+        object = None
+        object = bpy.context.active_object
+        if object == None:
+            self.report({"ERROR"}, "No Object selected. Please select the object to be saved.")
+            return {'CANCELLED'}
         try:
-            ID = bpy.context.selected_objects[0]["Z_ObjectID"]
+            ID = object["Z_ObjectID"]
         except:
-            self.report({'ERROR'}, f"{bpy.context.selected_objects[0].name} has no HD2 custom properties")
+            self.report({'ERROR'}, f"{object.name} has no HD2 custom properties")
             return{'CANCELLED'}
+        SwapID = ""
+        try:
+            SwapID = object["Z_SwapID"]
+        except:
+            self.report({'INFO'}, f"{object.name} has no HD2 Swap ID. Skipping Swap.")
         model = GetObjectsMeshData()
         BlenderOpts = bpy.context.scene.Hd2ToolPanelSettings.get_settings_dict()
         Entry = Global_TocManager.GetEntryByLoadArchive(int(ID), MeshID)
@@ -4271,20 +4283,20 @@ class SaveStingrayMeshOperator(Operator):
             try:
                 Entry.LoadedData.RawMeshes[mesh_index] = mesh
             except IndexError:
-                self.report({'ERROR'}, f"MeshInfoIndex for {bpy.context.selected_objects[0].name} exceeds the number of meshes")
+                self.report({'ERROR'}, f"MeshInfoIndex for {object.name} exceeds the number of meshes")
                 return{'CANCELLED'}
         for mesh_index, mesh in meshes.items():
             try:
-                if Entry.LoadedData.RawMeshes[mesh_index].DEV_BoneInfoIndex == -1 and bpy.context.selected_objects[0][
+                if Entry.LoadedData.RawMeshes[mesh_index].DEV_BoneInfoIndex == -1 and object[
                     'BoneInfoIndex'] > -1:
                     self.report({'ERROR'},
-                                f"Attempting to overwrite static mesh with {bpy.context.selected_objects[0].name}"
+                                f"Attempting to overwrite static mesh with {object[0].name}"
                                 f", which has bones. Check your MeshInfoIndex is correct.")
                     return{'CANCELLED'}
                 Entry.LoadedData.RawMeshes[mesh_index] = mesh
             except IndexError:
                 self.report({'ERROR'},
-                            f"MeshInfoIndex for {bpy.context.selected_objects[0].name} exceeds the number of meshes")
+                            f"MeshInfoIndex for {object[0].name} exceeds the number of meshes")
                 return{'CANCELLED'}
         wasSaved = Entry.Save(BlenderOpts=BlenderOpts)
         if wasSaved:
@@ -4294,6 +4306,9 @@ class SaveStingrayMeshOperator(Operator):
             self.report({"ERROR"}, f"Failed to save mesh {bpy.context.selected_objects[0].name}.")
             return{'CANCELLED'}
         self.report({'INFO'}, f"Saved Mesh Object ID: {self.object_id}")
+        if SwapID != "" and SwapID.isnumeric():
+                self.report({'INFO'}, f"Swapping Entry ID: {Entry.FileID} to: {SwapID}")
+                Entry.FileID = int(SwapID)
         return{'FINISHED'}
 
 class BatchSaveStingrayMeshOperator(Operator):
@@ -4316,14 +4331,33 @@ class BatchSaveStingrayMeshOperator(Operator):
             return {'CANCELLED'}
 
         IDs = []
+        IDswaps = {}
         for object in objects:
+            SwapID = ""
             try:
                 ID = object["Z_ObjectID"]
-                if ID not in IDs:
-                    IDs.append(ID)
+                try:
+                    SwapID = object["Z_SwapID"]
+                    IDswaps[SwapID] = ID
+                    PrettyPrint(f"Found Swap of ID: {ID} Swap: {SwapID}")
+                except:
+                    self.report({'INFO'}, f"{object.name} has no HD2 Swap ID. Skipping Swap.")
+                IDitem = [ID, SwapID]
+                if IDitem not in IDs:
+                    IDs.append(IDitem)
             except KeyError:
                 self.report({'ERROR'}, f"{object.name} has no HD2 custom properties")
                 return {'CANCELLED'}
+        swapCheck = {}
+        for IDitem in IDs:
+            ID = IDitem[0]
+            SwapID = IDitem[1]
+            if swapCheck.get(ID) == None:
+                swapCheck[ID] = SwapID
+            else:
+                if (swapCheck[ID] == "" and SwapID != "") or (swapCheck[ID] != "" and SwapID == ""):
+                    self.report({'ERROR'}, f"All Lods of object: {object.name} must have a swap ID! If you want to have an entry save to itself whilst swapping, set the SwapID to its own ObjectID.")
+                    return {'CANCELLED'}
         objects_by_id = {}
         for obj in objects:
             try:
@@ -4333,7 +4367,9 @@ class BatchSaveStingrayMeshOperator(Operator):
         MeshData = GetObjectsMeshData()
         BlenderOpts = bpy.context.scene.Hd2ToolPanelSettings.get_settings_dict()
         num_meshes = len(objects)
-        for ID in IDs:
+        for IDitem in IDs:
+            ID = IDitem[0]
+            SwapID = IDitem[1]
             Entry = Global_TocManager.GetEntryByLoadArchive(int(ID), MeshID)
             if Entry is None:
                 self.report({'ERROR'}, f"Archive for entry being saved is not loaded. Could not find custom property object at ID: {ID}")
@@ -4346,13 +4382,17 @@ class BatchSaveStingrayMeshOperator(Operator):
                 try:
                     Entry.LoadedData.RawMeshes[mesh_index] = mesh
                 except IndexError:
-                    self.report({'ERROR'},f"MeshInfoIndex of {mesh_index} for {objects_by_id[ID][mesh_index].name} exceeds the number of meshes")
+                    self.report({'ERROR'},f"MeshInfoIndex of {mesh_index} for {object.name} exceeds the number of meshes")
                     errors = True
                     num_meshes -= 1
             wasSaved = Entry.Save(BlenderOpts=BlenderOpts)
             if wasSaved:
                 if not Global_TocManager.IsInPatch(Entry):
                     Entry = Global_TocManager.AddEntryToPatch(int(ID), MeshID)
+
+                if SwapID != "" and SwapID.isnumeric():
+                    self.report({'INFO'}, f"Swapping Entry ID: {Entry.FileID} to: {SwapID}")
+                    Entry.FileID = int(SwapID)
             else:
                 self.report({"ERROR"}, f"Failed to save mesh with ID {ID}.")
                 num_meshes -= len(MeshData[ID])
