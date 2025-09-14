0a23f8bf1629cd5dd17f9e1e141d0e3e3e94eaaf
evie <evieshort65@gmail.com>
2025-09-14 18:25:11 -0400

    Only create new bone if not exist (now for LOD 0 branch too)

--- stingray/unit.py
+++ stingray/unit.py
@@ -1775,8 +1775,10 @@ def CreateModel(model, id, customization_info, bone_names, transform_info, bone_
                             boneName = Global_BoneNames[boneHash]
                         else:
                             boneName = str(boneHash)
-                        newBone = armature.edit_bones.new(boneName)
-                        newBone.tail = 0, 0.0000025, 0
+                        newBone = armature.edit_bones.get(boneName)
+                        if newBone is None:
+                            newBone = armature.edit_bones.new(boneName)
+                            newBone.tail = 0, 0.0000025, 0
                         bones[i] = newBone
                         boneParents[i] = boneParent
                         boneTransforms[newBone.name] = transform_info.Transforms[i]
