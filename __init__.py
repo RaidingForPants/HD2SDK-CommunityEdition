ac223393b0c9286cc34a1bca9ee2d057c8a6951f
BiscuitDev <86505354+Boxofbiscuits97@users.noreply.github.com>
2026-09-03 21:36:47 -0400

    Version Update

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
 bl_info = {
     "name": "Helldivers 2 SDK: Community Edition",
-    "version": (3, 9, 9),
+    "version": (3, 10, 0),
     "blender": (4, 0, 0),
     "category": "Import-Export",
 }
